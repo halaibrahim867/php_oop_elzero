b73f965f903cbd34b07fb69546f912934abbf569
hala ibrahim <halaibrahim867@gmail.com>
2023-10-12 02:18:01 +0300

    pratise about method with params

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ class AppDevice
     const OWNERNAME=5;
 
 
+    /*
     public function setOwnerName(){
 
         if(strlen($this->ownerName) < self::OWNERNAME){
@@ -18,26 +19,36 @@ class AppDevice
         }else{
             echo 'Your Name has been set';
         }
+    }*/
+
+    public function changeSpace($r,$in,$s, $c)
+    {
+        $this->ram=$r;
+        $this->inch=$in;
+        $this->space=$s;
+        $this->color=$c;
     }
 }
 
 $iphone6plus=new AppDevice();
 
+$iphone6plus->changeSpace('2GB','5 Inch','32GB','Gold');
+/*
 $iphone6plus->ram='2GB';
 $iphone6plus->inch='5 inch';
 $iphone6plus->space='32GB';
 $iphone6plus->color='Gold';
 $iphone6plus->ownerName='hi';
-$iphone6plus->setOwnerName();
-
+//$iphone6plus->setOwnerName();
+*/
 //to print const
-
+/*
 echo '<br>';
 echo AppDevice::OWNERNAME;
 echo '<br>';
 
 echo $iphone6plus::OWNERNAME;
-
+*/
 
 echo '<pre>';
 var_dump($iphone6plus);
@@ -45,11 +56,14 @@ echo  '</pre>';
 
 $iphone7plus=new AppDevice();
 
+$iphone7plus->changeSpace('4GB','7 Inch','128GB','Silver');
+
+/*
 $iphone7plus->ram='4GB';
 $iphone7plus->inch='6inch';
 $iphone7plus->space='256GB';
 $iphone7plus->color='Silver';
-
+*/
 echo '<pre>';
 var_dump($iphone7plus);
 echo  '</pre>';
