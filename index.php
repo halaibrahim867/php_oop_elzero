4cee38c74fa89940a45b4ff66e36b7ced33b4231
hala ibrahim <halaibrahim867@gmail.com>
2023-10-12 02:46:32 +0300

    practise Inheritance with Overriding

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ class AppDevice
     public $ownerName;
     //const OWNERNAME=5;
     private $lock;
+    public $name;
 
 
     /*
@@ -33,17 +34,28 @@ class AppDevice
     public function changeLock($l){
         $this->lock= sha1($l);
     }
+    public function sayHello($n){
+        $this->name=$n;
+        echo 'Welcome To: '.$n.'<br>';
+    }
 
 }
 
 class Sony extends AppDevice
 {
     public $camera='25MB';
+    public function sayHello($n){
+        $this->name=$n;
+        echo 'Welcome To: '.$n.' This Phone Has ' . $this->ram. ' Ram'.'<br>';
+    }
+
 }
+/*
 $iphone6plus=new AppDevice();
 
 $iphone6plus->changeSpace('2GB','5 Inch','32GB','Gold');
 $iphone6plus->changeLock('hala123');
+*/
 /*
 $iphone6plus->ram='2GB';
 $iphone6plus->inch='5 inch';
@@ -60,15 +72,14 @@ echo '<br>';
 
 echo $iphone6plus::OWNERNAME;
 */
-
+/*
 echo '<pre>';
 var_dump($iphone6plus);
 echo  '</pre>';
-
+*/
 $iphone7plus=new AppDevice();
-
 $iphone7plus->changeSpace('4GB','7 Inch','128GB','Silver');
-
+$iphone7plus->sayHello('iPhone7');
 /*
 $iphone7plus->ram='4GB';
 $iphone7plus->inch='6inch';
@@ -76,11 +87,13 @@ $iphone7plus->space='256GB';
 $iphone7plus->color='Silver';
 */
 echo '<pre>';
-var_dump($iphone7plus);
+print_r($iphone7plus);
 echo  '</pre>';
 
 $sony=new Sony();
+$sony->changeSpace('2GB','5Inch','64GB','Black');
+$sony->sayHello('Sony');
 echo '<pre>';
-var_dump($sony);
+print_r($sony);
 echo  '</pre>';
 
