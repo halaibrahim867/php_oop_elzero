2dfaa885e3187ce28b6a0fca8071a161778fa523
hala ibrahim <halaibrahim867@gmail.com>
2023-10-12 01:44:34 +0300

    add class and object

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class AppDevice
+{
+    public $ram;
+    public $inch;
+    public $space;
+    public $color;
+
+
+}
+
+$iphone6plus=new AppDevice();
+
+$iphone6plus->ram='2GB';
+$iphone6plus->inch='5 inch';
+$iphone6plus->space='32GB';
+$iphone6plus->color='Gold';
+
+
+$iphone7plus=new AppDevice();
+
+echo '<pre>';
+var_dump($iphone6plus);
+echo  '</pre>';
+
+
+$iphone7plus->ram='4GB';
+$iphone7plus->inch='6inch';
+$iphone7plus->space='256GB';
+$iphone7plus->color='Silver';
+
+echo '<pre>';
+var_dump($iphone7plus);
+echo  '</pre>';
+
+
